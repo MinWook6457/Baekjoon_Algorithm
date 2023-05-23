106a9806d981aa257ad34384d22429d57c9e4617
MinWook6457 <minwook6457@gmail.com>
2023-05-23 16:50:11 +0900

    Add 10989.c

---
+++ Baekjoon/10989.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+#define max 10001
+
+int main() {
+	int n;
+
+	scanf("%d", &n);
+
+	int count[max] = { 0, };
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		scanf("%d", &x);
+		count[x] = count[x] + 1;
+	}
+
+	for (int i = 1; i < max; i++) {
+		for (int j = 1; j <= count[i]; j++) {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
